3b6a9ba5711428d805f01eb6120e2a6dc259b6d8
EasyApk <easyapk@icloud.com>
2023-06-21 05:12:10 +0000

    update

--- Assets/Editor/UnityBuilderAction.cs
+++ Assets/Editor/UnityBuilderAction.cs
@@ -11,14 +11,14 @@ namespace UnityBuilderAction
     public static class BuildScript
     {
         private static readonly string Eol = Environment.NewLine;
-        private static string APP_NAME = "Test Saja";
-        private static string COMPANY_NAME = "EasyApk";
+        private static string APP_NAME = "moneymachine";
+        private static string COMPANY_NAME = "1687324321476948028543";
         private static string BUILD_VERSION_NAME = "1.0.0";
-        private static string PACKAGENAME = "com.janjijiwa.enakrasanya";
+        private static string PACKAGENAME = "com.money99.moneymachine";
         private static string PATH_KEYSTORE = "data.keystore";
-        private static string KEYSTORE_PASS = "123456";
-        private static string KEY_ALIAS_NAME = "test";
-        private static string KEY_ALIAS_PASS = "123456";
+        private static string KEYSTORE_PASS = "corp123";
+        private static string KEY_ALIAS_NAME = "mm99";
+        private static string KEY_ALIAS_PASS = "corp123";
 
         private static readonly string[] Secrets =
             {"androidKeystorePass", "androidKeyaliasName", "androidKeyaliasPass"};
@@ -37,7 +37,6 @@ namespace UnityBuilderAction
             PlayerSettings.companyName = COMPANY_NAME;
             PlayerSettings.bundleVersion = BUILD_VERSION_NAME;
             PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, PACKAGENAME);
-
 
             // Apply build target
             var buildTarget = (BuildTarget)Enum.Parse(typeof(BuildTarget), options["buildTarget"]);
